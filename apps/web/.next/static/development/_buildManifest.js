dcd9dc029745e74a7ad8b076e65749eaba5d2aab
Simao Miguel <simao.tc.miguel@gmail.com>
2026-06-08 21:17:57 +0100

    fix: export DataModule from data lib index
    Add missing DataModule export from @financial-hub/data index.ts
    This fixes module resolution errors in API where DataModule wasn't
    being found despite being defined in data.module.ts

--- apps/web/.next/static/development/_buildManifest.js
+++ apps/web/.next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/accounts":["static\u002Fchunks\u002Fpages\u002Faccounts.js"],"/analytics":["static\u002Fchunks\u002Fpages\u002Fanalytics.js"],sortedPages:["\u002F_app","\u002F_error","\u002Faccounts","\u002Fanalytics"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
